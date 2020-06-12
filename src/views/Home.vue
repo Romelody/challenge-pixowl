9256a2da824e786ff5dd1eb10ccc5c1936957774
Melody <romelody.calde@gmail.com>
2020-06-12 00:58:17 -0300

    discord cta fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,11 @@
     <ArtistQuotes :tabletView="tabletView" />
     <Join v-if="!tabletView" />
     <Footer v-if="!tabletView" />
+    <div class="discordCta">
+      <a href="https://discord.com/invite/X3Gw8NC"
+        ><img src="@/assets/img/discord-cta.svg" alt="Discord"
+      /></a>
+    </div>
   </div>
 </template>
 
@@ -35,3 +40,11 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+.discordCta {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  width: 75px;
+}
+</style>
